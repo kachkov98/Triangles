5564ced1d149c13ee4b5d062b2adb865b8859667
Sergei Kachkov <kachkov98@gmail.com>
2021-10-17 22:41:35 +0300

    Add test with large scene generation

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,21 @@
+#include "glm/gtc/random.hpp"
+#include "scene.hpp"
 #include <gtest/gtest.h>
+#include <iostream>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(Scene, RandomScene) {
+  constexpr unsigned N = 1000000;
+  scene::Scene triangles;
+  for (unsigned i = 0; i < N; ++i) {
+    glm::vec3 center = glm::linearRand(glm::vec3(-1000.f, -1000.f, -1000.f),
+                                       glm::vec3(1000.f, 1000.f, 1000.f));
+    triangles.emplace_back(center + glm::ballRand(1.f),
+                           center + glm::ballRand(1.f),
+                           center + glm::ballRand(1.f));
+  }
+  auto res = scene::findIntersectingTriangles(triangles);
+  for (auto idx : res)
+    std::cout << idx << " ";
+  std::cout << std::endl;
+  EXPECT_TRUE(true);
 }
