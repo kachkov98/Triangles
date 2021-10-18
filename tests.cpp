8a00c285f222a08eed5c449c659d436f154b54d6
Sergei Kachkov <kachkov98@gmail.com>
2021-10-18 23:25:40 +0300

    Add unit test for triangle intersection

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,29 @@
+#include "geometry.hpp"
 #include "glm/gtc/random.hpp"
 #include "scene.hpp"
 #include <gtest/gtest.h>
 #include <iostream>
+
+TEST(Geometry, Triangles) {
+  geom::Triangle tri{glm::vec3{5.f, 6.f, 7.f}, glm::vec3{6.f, 5.f, 4.f},
+                     glm::vec3{1.f, 2.f, 3.f}};
+  std::vector<std::pair<geom::Triangle, bool>> tris = {
+      {{glm::vec3{-1.f, 5.f, 0.f}, glm::vec3{2.f, 2.f, -3.f},
+        glm::vec3{5.f, 5.f, 0.f}},
+       false},
+      {{glm::vec3{-1.f, -1.f, 0.f}, glm::vec3{0.f, 1.f, 0.f},
+        glm::vec3{1.f, -1.f, 0.f}},
+       false},
+      {{glm::vec3{-1.f, -5.f, 0.f}, glm::vec3{2.f, -2.f, -3.f},
+        glm::vec3{5.f, -5.f, 0.f}},
+       false},
+      {{glm::vec3{5.f, 6.f, 7.f}, glm::vec3{6.f, 5.f, 4.f},
+        glm::vec3{1.f, 2.f, 3.f}},
+       true}};
+  std::vector<bool> answers = {false, false, false, true};
+  for (const auto &[other_tri, answer] : tris)
+    EXPECT_TRUE(geom::Intersects(tri, other_tri) == answer);
+}
 
 TEST(Scene, RandomScene) {
   constexpr unsigned N = 1000000;
